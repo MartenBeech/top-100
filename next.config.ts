f3ef9ffe1b0d475b9a5c2819074714c9c60f01b2
Morten Bech <mortenbech22@hotmail.com>
2026-01-15 09:54:20 +0100

    Fixed firebase deploy

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // Outputs a Single-Page Application (SPA)
-  distDir: "build", // Changes the build output directory to `build`
-};
-
-module.exports = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/player",
-        permanent: true,
-      },
-    ];
-  },
+  output: "export",
 };
 
 export default nextConfig;
